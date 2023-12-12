e4ede7ec071711fffb224da16027442a774a2241
Torazo5 <99123718+Torazo5@users.noreply.github.com>
2023-12-12 19:56:34 +0800

    test chat gbt afte rthsi

--- coord-inputs.py
+++ coord-inputs.py
@@ -15,19 +15,22 @@ def move_to_coords(x, y):
     velocity = 50
 
     if x > 0:
-        myDrone.send_rc_control(0, velocity, 0, 0)
-    else:
-        myDrone.send_rc_control(0, -velocity, 0, 0)
-    time.sleep((abs(x)*10)/velocity)
-    print(f'Waited for {(abs(x)*10)/velocity} seconds')
-
-    myDrone.send_rc_control(0, 0, 0, 0)
-
-    if y > 0:
         myDrone.send_rc_control(velocity, 0, 0, 0)
     else:
         myDrone.send_rc_control(-velocity, 0, 0, 0)
-    time.sleep((abs(y)*10)/velocity)
+    time.sleep((abs(x) * 10) / velocity)
+    time.sleep(0.3)
+    print(f'Waited for {(abs(x)*10)/velocity} seconds')
+
+    myDrone.send_rc_control(0, 0, 0, 0)
+    
+    time.sleep(0.5)
+    if y > 0:
+        myDrone.send_rc_control(0, velocity, 0, 0)
+    else:
+        myDrone.send_rc_control(0, -velocity, 0, 0)
+    time.sleep((abs(y) * 10) / velocity)
+    time.sleep(0.3)
     print(f'Waited for {(abs(y)*10)/velocity} seconds')
 
     myDrone.send_rc_control(0, 0, 0, 0)
@@ -44,8 +47,11 @@ while True:
     print('Next coordinates? (x y):')
     try:
         next_coords_x, next_coords_y = map(int, input().split())
+        print(f'x:{next_coords_x}, y:{next_coords_y}')
         move_to_coords(next_coords_x, next_coords_y)
-        time.sleep(2)
+
+        # Add a small delay before asking for the next coordinates
+        time.sleep(0.5)
     except ValueError:
         print('Invalid input. Please enter integer coordinates.')
 
